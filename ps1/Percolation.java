fb01552894ca1c2b8e128830064e0d46795c0cac
geekodour <hrishikeshbman@gmail.com>
2018-07-20 16:13:32 +0530

    wip

--- ps1/Percolation.java
+++ ps1/Percolation.java
@@ -1,21 +1,30 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
-    private final QuickFindUF uf[];
-    private int num;
+    private QuickFindUF uf;
+    private int size;
+
+    private int getIndex(int i, int j){
+      return (i-1)*size + (j-1);
+    }
 
     public Percolation(int n){          // create n-by-n grid, with all sites blocked
-      uf = new QuickFindUF[n*n + 2];    // +2 for vitual top and bottom
-      num = n;
+      uf = new QuickFindUF(n*n + 2);    // +2 for vitual top and bottom
+      size = n;
       int i = 0;
-      while(i < n){ }
     }
 
     public void open(int row, int col){       // open site (row, col) if it is not open already
-      row = row * num;
-      if()
+      int index = getIndex(row, col);
+      StdOut.print(index);
+      // row = row * size;
+      // connect to left if left is open
+      // connect to right if right is open
+      // connect to top if top is open
+      // connect to bottom if bottom is open
     }
 
     public boolean isOpen(int row, int col){  // is site (row, col) open?
@@ -36,5 +45,7 @@ public class Percolation {
     }
 
     public static void main(String[] args){    // test client (optional)
+      Percolation p = new Percolation(3);
+      p.open(2,3);
     }
 }
