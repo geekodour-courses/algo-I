bf7674975c90854f2192908caeb5e10abbcd2dbc
geekodour <hrishikeshbman@gmail.com>
2018-08-04 00:23:24 +0530

    wip

--- ps1/Percolation.java
+++ ps1/Percolation.java
@@ -1,94 +1,80 @@
 /******************************************************************************
- *  Compilation:  javac WeightedQuickUnionUF.java
- *  Execution:  java WeightedQuickUnionUF < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/15uf/tinyUF.txt
- *                https://algs4.cs.princeton.edu/15uf/mediumUF.txt
- *                https://algs4.cs.princeton.edu/15uf/largeUF.txt
+ *  Compilation:  javac Percolation.java
+ *  Execution:  java Percolation < input.txt
+ *  Dependencies: StdRandom StdStats QuickFindUF StdOut
  *
  *  Precolation
  *
  ******************************************************************************/
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.QuickFindUF;
+//import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Percolation {
-    private QuickFindUF uf;
-    private int size;
-    private int vtop = 0;
-    private int vbtm;
-    private boolean grid[][];
+public class Percolation_nw {
 
-    private int getIndex(int i, int j){
-      return (i-1)*size + j;
-    }
+  private boolean grid[][];
+  public WeightedQuickUnionUF uf;
+  private int openSites;
+  private int size;
 
-    private void validateIndex(int i, int j){
-      if(i <= 0 || i > size || j <= 0 || j > size) throw new java.lang.IllegalArgumentException();
-    }
+  private int encode(int i, int j){
+    return size*(i-1) + j;
+  }
 
-    //private void connectIfOpen(int i, int j){
-    //  uf.union(i, j);
-    //}
+  private void validate(int row, int col){
+    if ((row <= 0 && row > size) && (col <=0 && col > size)) throw new IllegalArgumentException();
+  }
 
-    public Percolation(int n){          // create n-by-n grid, with all sites blocked
-      uf = new QuickFindUF(n*n + 2);    // +2 for vitual top and bottom
-      size = n;
-      vbtm = size*size + 1;
-      grid = new boolean[n][n];
-    }
-
-    public void open(int row, int col){       // open site (row, col) if it is not open already
-
-      // validate row and col
-      validateIndex(row, col);
-      int index = getIndex(row, col);
-
-      // mark the site as open
-      grid[row-1][col-1] = true;
-
-
-      // if it is in the first row, connect to top
-      if (row == 1) { uf.union(index, vtop); }
-      if (row == size) { uf.union(index, vbtm); }
-
-      // call uniunion methods
-
-      if( uf.find(getIndex(row, col+1)) == getIndex(row, col+1) ){
+  private void connectIfOpen(int row, int col, int row_o, int col_o){
+    if ( (row > 0 && row <= size) && (col > 0 && col <= size) ) {
+      if(isOpen(row, col)){
+        uf.union(encode(row,col), encode(row_o, col_o));
       }
-      //connectIfOpen(getIndex(row, col+1), index); // right
-      //connectIfOpen(getIndex(row, col-1), index); // left
-      //connectIfOpen(getIndex(row-1, col), index); // top
-      //connectIfOpen(getIndex(row+1, col), index); // bottom
     }
+  }
 
-    public boolean isOpen(int row, int col){  // is site (row, col) open?
-      //if (i*j <= 0 && i*j > size*size){
-      //  throw new java.lang.IllegalArgumentException();
-      //}
-      return grid[row-1][col-1];
-    }
+  public Percolation_nw(int n){                 // create n-by-n grid, with all sites blocked
+    grid = new boolean[n][n];
+    uf = new WeightedQuickUnionUF(n*n+2);
+    openSites = 0;
+    size = n;
+  }
 
-    public boolean isFull(int row, int col){  // is site (row, col) full?
-      return uf.connected(vtop, getIndex(row, col));
-    }
+  public void open(int row, int col){          // open site (row, col) if it is not open already
+    validate(row, col);
+    grid[row-1][col-1] = true;
+    connectIfOpen(row-1, col, row, col);
+    connectIfOpen(row+1, col, row, col);
+    connectIfOpen(row, col-1, row, col);
+    connectIfOpen(row, col+1, row, col);
+    openSites += 1;
+  }
 
-    public int numberOfOpenSites(){           // number of open sites
-      return uf.count();
-    }
+  public boolean isOpen(int row, int col){     // is site (row, col) open?
+    validate(row, col);
+    return grid[row-1][col-1];
+  }
 
-    public boolean percolates(){              // does the system percolate?
-      return uf.connected(vtop, vbtm);
-    }
+  public boolean isFull(int row, int col){     // is site (row, col) full?
+    validate(row, col);
+    return uf.connected(0, encode(row, col));
+  }
 
-    public void main(String[] args){    // test client (optional)
-      Percolation p = new Percolation(3);
-      p.open(1,1);
-      p.open(1,2);
-      StdOut.print(uf.connected(1,2));
-      //StdOut.print(p.isFull(1,1));
-    }
+  public int numberOfOpenSites(){              // number of open sites
+    return openSites;
+  }
+
+  public boolean percolates(){                 // does the system percolate?
+    return uf.connected(0, size*size+1);
+  }
+
+  public static void main(String[] args){      // test client (optional)
+    Percolation_nw p = new Percolation_nw(3);
+    p.open(1,1);
+    p.open(1,2);
+    StdOut.println(p.uf.connected(1,2));
+  }
+
 }
